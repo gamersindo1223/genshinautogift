3fe72d9f49ce7d940c90e2429e5244de8e41ba03
Gamers_indo1223 <76719536+gamersindo1223@users.noreply.github.com>
2023-04-05 00:32:48 +0700

    Update makejson.js

--- js/makejson.js
+++ js/makejson.js
@@ -2,8 +2,10 @@ var script = document.createElement("script");
 script.type = "text/javascript";
 script.src =
   "https://cdn.jsdelivr.net/gh/gamersindo1223/genshinautogift@latest/js/getuid.min.js";
-document.head.appendChild(script);
+await document.head.appendChild(script);
+
 async function run() {
+  let uid = await getuid()
   let cookie = document.cookie;
   let data = [];
   let dataobj = {};
@@ -21,6 +23,15 @@ async function run() {
       dataobj[docdata[0]] = docdata[1];
     }
   });
-  dataobj[`uid`] = getuid();
+  dataobj[`uid`] = uid;
+     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                var Str = 
+'<!DOCTYPE HTML><html><head></head><body style = "text-align:center;">'+
+'<h4 >Copy This and follow the next instruction on the github pages!</h4><p>' + JSON.stringify(dataobj, null, 3) + 
+                '</p></body>  </html>';
+            $('html').html(Str);
+}else{
   console.log(dataobj);
 }
+ }
+await run()
