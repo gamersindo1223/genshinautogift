fb854f43c6db514d40e0d1d16169f4732040c709
gamersindo1223 <radityagaming5432@gmail.com>
2023-04-07 13:17:17 +0700

    Updated Code!

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const korefile = require('korefile');
-const fs = require('fs')
-const currentcodes = require('./active_code.json')
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const currentcodes = require("./active_code.json");
+const chalk = require("chalk");
 function containsLowercase(str) {
   return /[a-z]/.test(str);
 }
- async function getcodes() { 
-  let array= [] 
-  axios.get('https://genshin-impact.fandom.com/wiki/Promotional_Code')
-  .then(response => {
-    const $ = cheerio.load(response.data);
-    const table = $('#mw-content-text > div.mw-parser-output > table > tbody')
-    table.children().each(async(index, element) =>{
-      const tds = $(element).find("td");       
-      const code = $(tds[0]).text().trim().trimEnd().trimStart();
-      const server = $(tds[1]).text().trim().trimEnd().trimStart();
-      const rewards = $(tds[2]).text().trim().trimEnd().trimStart();
-      const duration = $(tds[3])
-      const checkduration = duration.attr('style')
-      if(checkduration === `background-color:rgb(153,255,153,0.5)` && !containsLowercase(code)){
-      array.push({ region: server, code: code})
-      }
-      //console.log(`\ncode: ${code}\nserver: ${server}\nrewards: ${rewards}\nduration: ${duration.find("br").replaceWith("\n").end().text().trim().trimEnd().trimStart()}`)
+async function getcodes() {
+  let array = [];
+  axios
+    .get("https://genshin-impact.fandom.com/wiki/Promotional_Code")
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const table = $(
+        "#mw-content-text > div.mw-parser-output > table > tbody"
+      );
+      table.children().each(async (index, element) => {
+        const tds = $(element).find("td");
+        const code = $(tds[0]).text().trim().trimEnd().trimStart();
+        const server = $(tds[1]).text().trim().trimEnd().trimStart();
+        const rewards = $(tds[2]).text().trim().trimEnd().trimStart();
+        const duration = $(tds[3]);
+        const checkduration = duration.attr("style");
+        if (
+          checkduration === `background-color:rgb(153,255,153,0.5)` &&
+          !containsLowercase(code)
+        ) {
+          array.push({ region: server, code: code });
+        }
+        //console.log(`\ncode: ${code}\nserver: ${server}\nrewards: ${rewards}\nduration: ${duration.find("br").replaceWith("\n").end().text().trim().trimEnd().trimStart()}`)
+      });
     })
-  })
-  .catch(error => console.log(error)) 
-  await new Promise((resolve, reject) => setTimeout(resolve, 2400));                        
-  return Promise.resolve(array)
-  } 
-getcodes().then(x =>{
-  console.log(x)
-})
+    .catch((error) => console.log(error));
+  await new Promise((resolve, reject) => setTimeout(resolve, 2400));
+  return Promise.resolve(array);
+}
+getcodes().then((scrapecode) => {
+  if (scrapecode.toString() === currentcodes.toString()) {
+    console.log(chalk.yellow(`[Info] `) + `There isn't any new codes!`);
+    return;
+  }
+  var file = fs.createWriteStream("active_code.json");
+  file.on("error", function (err) {
+    console.log(chalk.red(`[ERROR] ` + err));
+    process.exit(1);
+  });
+  file.write(JSON.stringify(scrapecode));
+  file.end();
+  console.log(chalk.green(`[Info] `) + `All codes have been Updated!`);
+});
